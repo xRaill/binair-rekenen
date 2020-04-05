46d08f0fca0cf79c77504c979daede26bd7ee821
Railly Koeiman <railly6@live.nl>
2020-04-05 21:56:32 +0200

    convert int to binary

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { Text, View, TextInput, StyleSheet } from 'react-native';
 
-const App = () => (
-    <Text>Hello world</Text>
-);
+const styles = StyleSheet.create({
+	input: {
+		backgroundColor: 'gray',
+		fontSize: 30
+	},
+	cubes : {
+		backgroundColor: 'red',
+		width: 200,
+		height: 200,
+		margin: 5
+	},
+	output: {
+		textAlign: 'center',
+		fontSize: 25,
+		fontWeight: "bold"
+	}
+});
+
+const App = () => {
+
+	const [inputNum, setInputNum] = useState('');
+	const [output, setOutput] = useState();
+
+	const calculate = useCallback(() => {
+		setOutput((inputNum >>> 0).toString(2));
+		setInputNum('');
+	}, [inputNum]);
+
+	return (
+		<View>
+			<TextInput
+				style={styles.input}
+				placeholder={'Placeholder'}
+				keyboardType={'number-pad'}
+				textAlign={'center'}
+				value={inputNum}
+				onChangeText={val => setInputNum(val)}
+				onSubmitEditing={calculate}
+			/>
+			<Text style={styles.output}>{output}</Text>
+		</View>
+	)
+};
 
 export default App;
