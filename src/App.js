ab1511a1e1b038843c248cd078c564c6fb3e720c
Railly Koeiman <railly6@live.nl>
2020-04-24 21:43:18 +0200

    Added labels to display

--- src/App.js
+++ src/App.js
@@ -21,7 +21,10 @@ const App = () => {
 		<View style={styles.container}>
 			<View style={styles.display}>
 				<Text style={styles.text}>{decimalNum ? decimalNum : null}</Text>
+				<Text style={styles.label}>Decimal</Text>
+				<View style={styles.separator} />
 				<Text style={styles.text}>{decimalNum ? binaryNum : null}</Text>
+				<Text style={styles.label}>Binary</Text>
 			</View>
 			<NumPad ref={numPadCallback} />
 		</View>
@@ -43,5 +46,16 @@ const styles = StyleSheet.create({
 		marginTop: 30,
 		color: 'white',
 		fontSize: 30,
+		textAlign: 'right',
+	},
+	label: {
+		borderTopWidth: 1,
+		borderTopColor: 'white',
+		color: 'white',
+		marginLeft: '30%',
+		paddingLeft: '5%',
+	},
+	separator: {
+		height: 30,
 	},
 });
